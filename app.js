ff675c6ac5ae626130ab26996cdf0b2e37735cd7
azadtamal <mrazadt@gmail.com>
2021-08-25 18:40:36 +0600

    function

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-function getSum(a, b = 9) {
-  return a + b;
+// function getSum(a, b = 9) {
+//   return a + b;
+// }
+// console.log(getSum(2, 10));
+
+// const y = (x) => x * x;
+// const x = y(5);
+// console.log(x);
+
+// const name = "Hero";
+// const greetings = "Welcome Home!" + " " + name;
+
+// const cars = ["toyota", "honda", "mercedes"];
+// const newCars = [...cars, "ferrari"];
+// console.log(newCars);
+
+// const azad1 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(azad1(12, 2));
+// const azad2 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(azad2(12, 2));
+// const azad3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(azad3(12, 2));
+
+// const friends = ["azad", " abid", "tamal"];
+
+function min(nums) {
+  return Math.min(nums);
 }
-console.log(getSum(2, 10));
 
-const y = (x) => x * x;
-const x = y(5);
-console.log(x);
-
-const name = "Hero";
-const greetings = "Welcome Home!" + " " + name;
-
-const cars = ["toyota", "honda", "mercedes"];
-const newCars = [...cars, "ferrari"];
-console.log(newCars);
-
-const azad1 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(azad1(12, 2));
-const azad2 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(azad2(12, 2));
-const azad3 = (num1, num2) => {
-  return num1 + num2;
-};
-
-console.log(azad3(12, 2));
-
-const friends = ["azad", " abid", "tamal"];
+console.log(min([1, 3, 2]));
