dc963c2e6abb7c2a68a52713342444617e7a78df
azadtamal <mrazadt@gmail.com>
2021-08-31 23:09:17 +0600

    object

--- app2.js
+++ app2.js
@@ -11,25 +11,28 @@
 // const getGirlFriend = (name = "chokina") => "name";
 // console.log(getGirlFriend());
 
-const azad = {
-  name: "Azad Tamal",
-  education: "Honours",
-  myArray: ["pubg", "coc", "pwi"],
-  roll: 8,
-  skils: "wev dev",
-  taka: 5000,
-  badjaiboi: function () {
-    return this.skils;
-  },
-  address: {
-    home: "gazi bari",
-    village: "nandopara",
-    dis: "barishal",
-    CAddress: "Dhaka",
-  },
-};
-const tamal = azad.badjaiboi();
-// console.log(tamal);
+// const azad = {
+//   name: "Azad Tamal",
+//   education: "Honours",
+//   myArray: ["pubg", "coc", "pwi"],
+//   roll: 8,
+//   skils: "wev dev",
+//   taka: 5000,
+//   badjaiboi: function () {
+//     return this.skils;
+//   },
+//   address: {
+//     home: "gazi bari",
+//     village: "nandopara",
+//     dis: "barishal",
+//     CAddress: "Dhaka",
+//   },
+// };
+// const tamal = azad.badjaiboi();
+// // console.log(tamal);
 
-const azadt = `name : ${azad.name} , address : ${azad.address.home} , game:${azad.myArray[1]}`;
-console.log(azadt);
+// const azadt = `name : ${azad.name} , address : ${azad.address.home} , game:${azad.myArray[1]}`;
+// console.log(azadt);
+
+const azad = () => 89;
+console.log(azad());
