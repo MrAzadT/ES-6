5e972d958afdead6c1956e6f145aeded1f594224
azadtamal <mrazadt@gmail.com>
2021-08-31 11:28:48 +0600

    app

--- app.js
+++ app.js
@@ -1,108 +1,108 @@
-// function getSum(a, b = 9) {
-//   return a + b;
-// }
-// console.log(getSum(2, 10));
+function getSum(a, b = 9) {
+  return a + b;
+}
+console.log(getSum(2, 10));
 
-// const y = (x) => x * x;
-// const x = y(5);
-// console.log(x);
+const y = (x) => x * x;
+const x = y(5);
+console.log(x);
 
-// const name = "Hero";
-// const greetings = "Welcome Home!" + " " + name;
+const name = "Hero";
+const greetings = "Welcome Home!" + " " + name;
 
-// const cars = ["toyota", "honda", "mercedes"];
-// const newCars = [...cars, "ferrari"];
-// console.log(newCars);
+const cars = ["toyota", "honda", "mercedes"];
+const newCars = [...cars, "ferrari"];
+console.log(newCars);
 
-// const azad1 = (num1, num2) => {
-//   return num1 + num2;
-// };
-// console.log(azad1(12, 2));
-// const azad2 = (num1, num2) => {
-//   return num1 + num2;
-// };
-// console.log(azad2(12, 2));
-// const azad3 = (num1, num2) => {
-//   return num1 + num2;
-// };
+const azad1 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(azad1(12, 2));
+const azad2 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(azad2(12, 2));
+const azad3 = (num1, num2) => {
+  return num1 + num2;
+};
 
-// console.log(azad3(12, 2));
+console.log(azad3(12, 2));
 
-// const friends = ["azad", " abid", "tamal"];
+const friends = ["azad", " abid", "tamal"];
 
-// function min(nums) {
-//   return Math.min(nums);
-// }
+function min(nums) {
+  return Math.min(nums);
+}
 
-// console.log(min([1, 3, 2]));
+console.log(min([1, 3, 2]));
 
-// const cube = (x) => x * x * x;
-// console.log(cube(2));
+const cube = (x) => x * x * x;
+console.log(cube(2));
 
-// const [a, b] = [1, 2, 3, 4, 45, 5];
-// console.log(a + b);
+const [a, b] = [1, 2, 3, 4, 45, 5];
+console.log(a + b);
 
-// const { x, y, z } = { x: 1, y1: 2, z: 3 };
-// console.log(y);
+const { x, y, z } = { x: 1, y1: 2, z: 3 };
+console.log(y);
 
-// const nums = [1, 2, 3, 4, 5];
-// let output = nums.filter((n) => n % 2);
-// console.log(output);
+const nums = [1, 2, 3, 4, 5];
+let output = nums.filter((n) => n % 2);
+console.log(output);
 
-// const friends = ["Moushumi", "Misha", "Manna", "mimi", "mahiya"];
+const friends = ["Moushumi", "Misha", "Manna", "mimi", "mahiya"];
 
-// const product = { name: "Laptop", model: "Yoga", price: 49000, dusk: "512SSD" };
+const product = { name: "Laptop", model: "Yoga", price: 49000, dusk: "512SSD" };
 
-// let firstName = "John";
-// let lastName = "Doe";
+let firstName = "John";
+let lastName = "Doe";
 
-// let text = `Welcome ${firstName}, ${lastName}!`;
-// console.log(text);
+let text = `Welcome ${firstName}, ${lastName}!`;
+console.log(text);
 
-// const azad = (p, q) => {
-//   return p * q;
-// };
-// console.log(azad(2, 4));
-// ---------------
-// const azadT = (p, q, w) => {
-//   return p + q + w;
-// };
-// const tamal = [2, 3, 8];
+const azad = (p, q) => {
+  return p * q;
+};
+console.log(azad(2, 4));
+---------------
+const azadT = (p, q, w) => {
+  return p + q + w;
+};
+const tamal = [2, 3, 8];
 
-// console.log(azadT(...tamal));
+console.log(azadT(...tamal));
 
-// let p = "JavaScript";
-// let q = p;
-// p = "React";
-// console.log(q);
+let p = "JavaScript";
+let q = p;
+p = "React";
+console.log(q);
 
-// const isTrue = "false";
+const isTrue = "false";
 
-// if (!isTrue) {
-//   console.log("hello");
-// } else {
-//   console.log("world");
-// }
+if (!isTrue) {
+  console.log("hello");
+} else {
+  console.log("world");
+}
 
-// function sum(p, q) {
-//   console.log(p + q);
-// }
+function sum(p, q) {
+  console.log(p + q);
+}
 
-// const result = sum(2, 3);
+const result = sum(2, 3);
 
-// if ("2" === 2) {
-//   console.log("Inside if");
-// } else {
-//   console.log("Inside else");
-// }
+if ("2" === 2) {
+  console.log("Inside if");
+} else {
+  console.log("Inside else");
+}
 
-// function work(x, y = 4){
+function work(x, y = 4){
 
-//   return x + y;
+  return x + y;
 
-//   }
+  }
 
-//   console.log(work(32);
+  console.log(work(32);
 
 const kala = {
   name: "saif",
@@ -113,12 +113,12 @@ const kala = {
   location: "dhaka",
 };
 Object.freeze(kala);
-// const kala2 = Object.entries(kala);
-// const kala1 = Object.keys(kala);
-// const kala3 = Object.values(kala);
-// const kala4 = Object.values(kala);
+const kala2 = Object.entries(kala);
+const kala1 = Object.keys(kala);
+const kala3 = Object.values(kala);
+const kala4 = Object.values(kala);
 delete kala.kam;
 console.log(kala);
-// console.log(kala1);
-// console.log(kala2);
-// console.log(kala3);
+console.log(kala1);
+console.log(kala2);
+console.log(kala3);
