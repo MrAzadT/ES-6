3f8eb95e4685cdbad27387c193a3b1f2703fd479
azadtamal <mrazadt@gmail.com>
2021-08-31 22:09:32 +0600

    function

--- app2.js
+++ app2.js
@@ -1,12 +1,25 @@
-// const obj ={a:1, b:7, c:3, length:2};
+// // const obj ={a:1, b:7, c:3, length:2};
 
-// console.log(Object.keys(obj).length);
+// // console.log(Object.keys(obj).length);
 
-// const obj1 = { module: 35, video: 2 };
+// // const obj1 = { module: 35, video: 2 };
 
-// const obj2 = { module: 35, video: 2 };
+// // const obj2 = { module: 35, video: 2 };
 
-// console.log(obj1 === obj2);
+// // console.log(obj1 === obj2);
 
-const getGirlFriend = (name = "chokina") => "name";
-console.log(getGirlFriend());
+// const getGirlFriend = (name = "chokina") => "name";
+// console.log(getGirlFriend());
+
+const azad = {
+  name: "Azad Tamal",
+  education: "Honours",
+  roll: 8,
+  skils: "wev dev",
+  taka: 5000,
+  badjaiboi: function () {
+    return this.name;
+  },
+};
+const tamal = azad.badjaiboi();
+console.log(tamal);
