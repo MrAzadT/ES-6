406ea3c0160af6bf918f8b927dc0d7e513f0d404
azadtamal <mrazadt@gmail.com>
2021-09-05 23:32:43 +0600

    set

--- app4.js
+++ app4.js
@@ -2,13 +2,29 @@
 //   console.log("azad");
 // }, 10000);
 
-const getTime = () => {
-  let time = new Date();
-  document.getElementById("time").innerHTML = time;
-};
-setInterval(getTime, 2000);
+// const getTime = () => {
+//   let time = new Date();
+//   document.getElementById("time").innerHTML = time;
+// };
+// setInterval(getTime, 2000);
 
 // ----------
 // setInterval(function () {
 //   alert("Hello");
 // }, 1000);
+
+// let userInput = prompt();
+// if (userInput) {
+//   alert(userInput);
+// }
+
+let sign = prompt("What's your sign?");
+
+if (sign.toLowerCase() == "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
+
+// sign = window.prompt();
+// sign = prompt();
+// sign = window.prompt("Are you feeling lucky");
+// sign = window.prompt("Are you feeling lucky", "sure");
