5f2a9e75e6d816e3ed4acd7b3fd5d77de1ee0769
azadtamal <mrazadt@gmail.com>
2021-09-05 23:10:45 +0600

    set

--- app4.js
+++ app4.js
@@ -1,3 +1,9 @@
-setTimeout(() => {
-  console.log("azad");
-}, 10000);
+// setTimeout(() => {
+//   console.log("azad");
+// }, 10000);
+
+const getTime = () => {
+  let time = new Date();
+  document.getElementById("time").innerHTML = time;
+};
+setInterval(getTime, 100);
