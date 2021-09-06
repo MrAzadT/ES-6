3c82633100d5f4575060b665bf937b55648f3f3e
azadtamal <mrazadt@gmail.com>
2021-09-06 21:40:14 +0600

    set

--- app4.js
+++ app4.js
@@ -46,6 +46,12 @@
 // }
 // =============
 
-let mySet = new set();
-mySet.add(5);
+let mySet = new Set();
+
+mySet.add(1); // Set [ 1 ]
+mySet.add(5); // Set [ 1, 5 ]
+mySet.add(5); // Set [ 1, 5 ]
+mySet.add("some text"); // Set [ 1, 5, 'some text' ]
+let o = { a: 1, b: 2 };
+mySet.add(o);
 console.log(mySet);
